b3848d1028f15be7b5baa2028b266c145e262b7a
Dineshranwa <46679220+Dineshranwa@users.noreply.github.com>
2026-06-26 19:45:45 +0530

    Done added dbcontext in program.cs file

--- EmployeeManagement.Api/Data/AppliactionDbContext.cs
+++ EmployeeManagement.Api/Data/AppliactionDbContext.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 namespace EmployeeManagement.Api.Data
 {
     public class AppliactionDbContext : DbContext
     {
+        public AppliactionDbContext(DbContextOptions<AppliactionDbContext> options) : base(options) { 
+        }
     }
 }
--- EmployeeManagement.Api/Program.cs
+++ EmployeeManagement.Api/Program.cs
@@ -1,3 +1,6 @@
+using EmployeeManagement.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,19 +9,28 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
+// To Swagger in api 
 builder.Services.AddSwaggerGen();
+
+//To create dbContext 
+builder.Services.AddDbContext<AppliactionDbContext>(x =>
+{
+    x.UseSqlServer( // to tell that which database we are going to use , sql , mysql and etc.
+    builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{   
+{
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+// To swagger api in interface  or to show swagger interface. 
 app.UseSwagger();
 app.UseSwaggerUI();
 
