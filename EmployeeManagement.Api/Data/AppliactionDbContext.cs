5ff93a15762c982d110829b02652c2dc50599806
Dineshranwa <46679220+Dineshranwa@users.noreply.github.com>
2026-06-29 22:00:17 +0530

    remarks

--- EmployeeManagement.Api/Data/AppliactionDbContext.cs
+++ EmployeeManagement.Api/Data/AppliactionDbContext.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Options;
 namespace EmployeeManagement.Api.Data
 {
     public class AppliactionDbContext : DbContext
-    {// db context has been added here. finally 
+    {// db context has been added here. finally thank you  
         public AppliactionDbContext(DbContextOptions<AppliactionDbContext> options) : base(options) { 
         }
     }
