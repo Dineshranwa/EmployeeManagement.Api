48a505361be7a13f2c4178ba95c0eac97d5863af
Dineshranwa <46679220+Dineshranwa@users.noreply.github.com>
2026-06-30 08:21:09 +0530

    Properties has been added in employee model class

--- EmployeeManagement.Api/Models/Employee.cs
+++ EmployeeManagement.Api/Models/Employee.cs
@@ -2,5 +2,20 @@
 {
     public class Employee
     {
+
+        public int Id { get; set; }
+        public string EmployeeCode { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime JoiningDate { get; set; }
+        public DateTime DateOfLeaving { get; set; }
+        public string EmployementStatus { get; set; }
+        public int DepartmentId { get; set; }
+        public int DesignationId { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
     }
 }
