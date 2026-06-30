a08e1797dfaff90f7aef083f175194b095c728d4
Dineshranwa <46679220+Dineshranwa@users.noreply.github.com>
2026-06-30 08:09:26 +0530

    In deparment model class property has been added.

--- EmployeeManagement.Api/Models/Department.cs
+++ EmployeeManagement.Api/Models/Department.cs
@@ -2,5 +2,11 @@
 {
     public class Department
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
     }
 }
