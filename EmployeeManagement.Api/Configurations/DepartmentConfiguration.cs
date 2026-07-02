8bb82f9e7f8efbfda6d0a3c9621bb4bbbd2e3635
Dineshranwa <46679220+Dineshranwa@users.noreply.github.com>
2026-07-02 08:01:22 +0530

    Configuration class has been created for department as well

--- EmployeeManagement.Api/Configurations/DepartmentConfiguration.cs
+++ EmployeeManagement.Api/Configurations/DepartmentConfiguration.cs
@@ -1,6 +1,12 @@
-﻿namespace EmployeeManagement.Api.Configurations
+﻿using Microsoft.EntityFrameworkCore;
+using EmployeeManagement.Api.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EmployeeManagement.Api.Configurations
 {
-    public class DepartmentConfiguration
+    public class DepartmentConfiguration :IEntityTypeConfiguration<Department>
     {
+        public void Configure(EntityTypeBuilder<Department> builder) { 
+        }
     }
 }
