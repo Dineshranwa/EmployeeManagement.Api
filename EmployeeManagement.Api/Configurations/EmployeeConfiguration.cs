ebe63e0bc9bd76734c18b20adfd708c5355e41fa
Dineshranwa <46679220+Dineshranwa@users.noreply.github.com>
2026-07-04 23:43:04 +0530

    Added configuration of EmployeeConfiguration

--- EmployeeManagement.Api/Configurations/EmployeeConfiguration.cs
+++ EmployeeManagement.Api/Configurations/EmployeeConfiguration.cs
@@ -1,6 +1,12 @@
-﻿namespace EmployeeManagement.Api.Configurations
+﻿using EmployeeManagement.Api.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EmployeeManagement.Api.Configurations
 {
     public class EmployeeConfiguration
     {
+        public void Configure(EntityTypeBuilder<EmployeeConfiguration> builder)
+        {
+        }
     }
 }
