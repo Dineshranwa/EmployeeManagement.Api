734f28dc8cc61bed8776c46f904c7bd76262eadf
Dineshranwa <46679220+Dineshranwa@users.noreply.github.com>
2026-07-06 23:58:21 +0530

    done

--- EmployeeManagement.Api/Configurations/EmployeeConfiguration.cs
+++ EmployeeManagement.Api/Configurations/EmployeeConfiguration.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 namespace EmployeeManagement.Api.Configurations
 {
     public class EmployeeConfiguration
-    {// Employee configuration has been created 
+    {// Employee configuration has been created done
         public void Configure(EntityTypeBuilder<EmployeeConfiguration> builder)
         {
         }
