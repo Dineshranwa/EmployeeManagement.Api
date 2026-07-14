c435afe9f2a594d077dc48936c8a47110544b7a9
Dineshranwa <46679220+Dineshranwa@users.noreply.github.com>
2026-07-14 08:17:59 +0530

    Employee configuration table has started by using fluent api.

--- EmployeeManagement.Api/Configurations/EmployeeConfiguration.cs
+++ EmployeeManagement.Api/Configurations/EmployeeConfiguration.cs
@@ -1,12 +1,16 @@
-﻿using EmployeeManagement.Api.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using EmployeeManagement.Api.Models;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace EmployeeManagement.Api.Configurations
 {
-    public class EmployeeConfiguration
-    {// done
+    public class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+    {
         public void Configure(EntityTypeBuilder<Employee> builder)
         {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.EmployeeCode).HasMaxLength(11).IsRequired();
+            builder.Property(e => e.FirstName).HasMaxLength(121).IsRequired();
         }
     }
 }
