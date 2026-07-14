77723496807ae7c4466dcf6b32a49500de6918a2
Dineshranwa <46679220+Dineshranwa@users.noreply.github.com>
2026-07-14 22:37:35 +0530

    Added the complete columns of employee table using fluent api

--- EmployeeManagement.Api/Data/AppliactionDbContext.cs
+++ EmployeeManagement.Api/Data/AppliactionDbContext.cs
@@ -4,20 +4,37 @@ using Microsoft.Extensions.Options;
 namespace EmployeeManagement.Api.Data
 {
     public class AppliactionDbContext : DbContext
-    {  
-        public AppliactionDbContext(DbContextOptions<AppliactionDbContext> options) : base(options) { 
+    {
+        public AppliactionDbContext(DbContextOptions<AppliactionDbContext> options) : base(options)
+        {
         }
 
         public DbSet<Employee> Employees { get; set; }
         public DbSet<Designation> Designations { get; set; }
-        public DbSet<Department> Departments{ get; set; }
+        public DbSet<Department> Departments { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<Employee>(entity =>{
-                entity.Property(e =>e.FirstName ).HasMaxLength(121).IsRequired();
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                
+                entity.Property(e => e.EmployeeCode).HasMaxLength(10).IsRequired();
+                entity.Property(e => e.FirstName).HasMaxLength(100).IsRequired();
+                entity.Property(e => e.LastName).HasMaxLength(100).IsRequired();
+                entity.Property(e => e.Email).HasMaxLength(200).IsRequired();
+                entity.Property(e => e.PhoneNumber).HasMaxLength(15).IsRequired();
+                entity.Property(e => e.JoiningDate).IsRequired();
+                entity.Property(e => e.DateOfLeaving);
+                entity.Property(e => e.EmployementStatus).HasMaxLength(50);
+                entity.Property(e => e.DepartmentId);
+                entity.Property(e => e.DesignationId).HasMaxLength(200).IsRequired();
+                entity.Property(e => e.IsActive).HasDefaultValue(false);
+                entity.Property(e => e.CreatedDate).HasDefaultValue(DateTime.Now).IsRequired();
+                entity.Property(e => e.ModifiedDate);
+
+
             });
         }
     }
